e1e1bcf9d17fa7ffcebab8cff427d287e47f5f00
SaturnEFTPMC <zackhhi@gmail.com>
2022-04-15 14:07:28 -0400

    Image Fix

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
       </Head>
         <div id="base">
-        <Image id="image" src="/illustration-hero.svg" alt="Person Dancing"></Image>
+        <img id="image" src="/illustration-hero.svg" alt="Person Dancing"></img>
           <div id="text">
           <p id="title">Order Summary</p>
           <p className="paleblue">You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
           <div id="price">
-          <Image id="music" src="icon-music.svg" alt="Music"></Image>
+          <img id="music" src="icon-music.svg" alt="Music"></img>
             <div id="undertitle">Annual Plan<p id="cost" className="paleblue">$59.99/year</p></div>
             
             <a id="change">Change</a>
